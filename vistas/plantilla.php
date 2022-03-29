f866eb9963b84f230c672b0810512b29dbb4e7e9
RaymundoDev <raymundodev4@gmail.com>
2022-03-28 20:24:47 -0600

    Cabecera responsiva

---
+++ vistas/plantilla.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minumum-scale=1.0, maximum-scale=1.0, user-scalanñe=no">
+	<meta name="title" content="Tienda Virtual">
+
+
+	<link rel="stylesheet" href="vistas/css/cabezote.css">
+	<link rel="stylesheet" href="vistas/css/plantillas.css">
+	<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+
+	<meta name="description" content="Lorem Ipsum Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Dolores distinctio repudiandae expedita labore quaerat placeat consequatur adipisci omnis minima soluta!">
+
+	<meta name="keyword" content="Lorem Ipsum Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Dolores distinctio repudiandae expedita labore quaerat placeat consequatur adipisci omnis minima soluta!">
+
+	<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+	<link rel="stylesheet" href="vistas/css/plantilla.css">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Fredoka&family=Open+Sans&family=Poppins:wght@200;300;400&display=swap" rel="stylesheet">
+
+
+<title>Tienda Virtual</title>
+
+</head>
+<body>
+
+<?php
+
+# ================================
+# =          CABEZOTE           =
+# ================================
+
+include "modulos/cabezote.php";
+
+
+?>
+
+<script src="vistas/js/plugins/jquery.min.js"></script>
+<script src="vistas/js/plugins/bootstrap.min.js"></script>
+
+</body>
+</html>
